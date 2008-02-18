871cbdeabf06ec4a5965f310b9d25b71731e11a0
AngelCarpintero <AngelCarpintero@4132a67d-e308-0410-984c-893ed6249e5e>
2008-02-18 06:05:57 +0000

    Change version name in trunk , Added svn revision running autoconf

---
+++ version.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SNV_VERSION=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+test $SNV_VERSION || SNV_VERSION=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
+test $SNV_VERSION || SNV_VERSION=UNKNOWN
+echo -n "trunk-r$SNV_VERSION"
