b5f2e7e18b2f2a151501f1f0570a3e6bf2eef85f
AngelCarpintero <AngelCarpintero@4132a67d-e308-0410-984c-893ed6249e5e>
2008-10-13 00:26:18 +0000

    Fix rights of version.sh that breaks versions for some building systems that runs autoconf & configure

--- version.sh
+++ version.sh
@@ -3,4 +3,6 @@
 SNV_VERSION=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $SNV_VERSION || SNV_VERSION=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
 test $SNV_VERSION || SNV_VERSION=UNKNOWN
-echo -n "3.2.11"
+#echo -n "3.2.11"
+echo -n "3.2-branch-r$SNV_VERSION"
+
