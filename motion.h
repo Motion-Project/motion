bbd2f3f81393e5f8530c6b1e7c0302bfd3516d7b
David Fries <David@Fries.net>
2014-08-14 20:43:19 -0500

    redo watchdog termination logic, wait for the thread to cleanup
    I apparently have some marginal USB hardware and motion has been
    crashing every couple of days in the memcpy at the end of alg.c as
    both cnt->imgs.ref and cnt->imgs.image_virgin were NULL pointers.
    This was just after the main thread watchdog timer called
    pthread_cancel on that thread.  The problem is pthread_cancel can't
    possibly kill a thread running on another CPU atomically, although in
    this case it's a single core processor and I think pthread_cancel was
    releasing it from the ioctl and it would then run to completition.
    
    This modifies the code to not cleanup that content's resources until
    that thread is no longer running.  If it runs to completition a normal
    restart will happen, if it doesn't the main thread will check once a
    second until it no longer is running, cleanup and restart, but it
    can't cleanup with that thread running.

--- motion.h
+++ motion.h
@@ -149,6 +149,7 @@
                                      */
 
 #define WATCHDOG_TMO            30   /* 30 sec max motion_loop interval */
+#define WATCHDOG_KILL          -60   /* -60 sec grace period before calling thread cancel */
 #define WATCHDOG_OFF          -127   /* Turn off watchdog, used when we wants to quit a thread */
 
 #define CONNECTION_KO           "Lost connection"
