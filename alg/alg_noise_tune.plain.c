e1db59a000a2caf45dae8a169ad27795b10216a9
Alfred Klomp <git@alfredklomp.com>
2014-09-02 21:52:58 +0200

    alg_noise_tune: change ints to unsigned for 10% speed improvement

--- alg/alg_noise_tune.plain.c
+++ alg/alg_noise_tune.plain.c
@@ -1,7 +1,3 @@
-#ifndef ABS
-#define ABS(x)             ((x) < 0 ? -(x) : (x))
-#endif
-
 /**
  * alg_noise_tune_plain
  *
@@ -9,22 +5,21 @@
 static void alg_noise_tune_plain(struct context *cnt, unsigned char *new)
 {
     struct images *imgs = &cnt->imgs;
-    int i;
     unsigned char *ref = imgs->ref;
-    int diff, sum = 0, count = 0;
+    unsigned int sum = 0, count = 0;
     unsigned char *mask = imgs->mask;
     unsigned char *smartmask = imgs->smartmask_final;
 
-    i = imgs->motionsize;
+    int i = imgs->motionsize;
 
     for (; i > 0; i--) {
-        diff = ABS(*ref - *new);
+        unsigned char absdiff = (*ref > *new) ? (*ref - *new) : (*new - *ref);
 
         if (mask)
-            diff = ((diff * *mask++) / 255);
+            absdiff = ((absdiff * *mask++) / 255);
 
         if (*smartmask) {
-            sum += diff + 1;
+            sum += absdiff + 1;
             count++;
         }
 
