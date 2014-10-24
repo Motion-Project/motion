fb9befa241763d557f13f6d26b19ce9f927495ff
Mr Dave <motionmrdave@gmail.com>
2014-10-23 20:23:46 -0700

    Revise version script

--- version.sh
+++ version.sh
@@ -3,6 +3,9 @@
 SNV_VERSION=`cd "$1" && LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
 test $SNV_VERSION || SNV_VERSION=`cd "$1" && grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
 test $SNV_VERSION || SNV_VERSION=UNKNOWN
-SNV_VERSION=`git show -s --format=%h`
-echo -n "3.4.0-Git-$SNV_VERSION"
+GITDIR=".git"
+if [ -d "$GITDIR" ]; then 
+  SNV_VERSION=`git show -s --format=%h`
+fi
+echo -n "Unofficial-Git-$SNV_VERSION"
 
