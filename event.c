3e99410c5f8dc25eba1c4ec8fb1bee1a0bff40a7
jdodgen <jim@dodgen.us>
2015-05-17 09:12:09 -0700

    Tab to space conversion

--- event.c
+++ event.c
@@ -349,17 +349,17 @@ static void event_image_snapshot(struct context *cnt, int type ATTRIBUTE_UNUSED,
             unsigned char *img, char *dummy1 ATTRIBUTE_UNUSED,
             void *dummy2 ATTRIBUTE_UNUSED, struct tm *currenttime_tm)
 {
-	char fullfilename[PATH_MAX];
-	char filename[PATH_MAX];
-	char filepath[PATH_MAX];
-	int offset = 0;
-	int len = strlen(cnt->conf.snappath);
-	if (len >= 9)
-	{
-		offset = len - 8;
-	}
-	if (strcmp(cnt->conf.snappath+offset, "lastsnap"))
-		char linkpath[PATH_MAX];
+    char fullfilename[PATH_MAX];
+    char filename[PATH_MAX];
+    char filepath[PATH_MAX];
+    int offset = 0;
+    int len = strlen(cnt->conf.snappath);
+
+    if (len >= 9)
+        offset = len - 8;
+
+    if (strcmp(cnt->conf.snappath+offset, "lastsnap")) {
+        char linkpath[PATH_MAX];
         const char *snappath;
         /*
          *  conf.snappath would normally be defined but if someone deleted it by control interface
@@ -388,9 +388,9 @@ static void event_image_snapshot(struct context *cnt, int type ATTRIBUTE_UNUSED,
             return;
         }
     } else {
-		mystrftime(cnt, filepath, sizeof(filepath), cnt->conf.snappath, currenttime_tm, NULL, 0);
-		snprintf(filename, PATH_MAX, "%s.%s", filepath, imageext(cnt));
-		snprintf(fullfilename, PATH_MAX, "%s/%s", cnt->conf.filepath, filename);
+        mystrftime(cnt, filepath, sizeof(filepath), cnt->conf.snappath, currenttime_tm, NULL, 0);
+        snprintf(filename, PATH_MAX, "%s.%s", filepath, imageext(cnt));
+        snprintf(fullfilename, PATH_MAX, "%s/%s", cnt->conf.filepath, filename);
         remove(fullfilename);
         put_picture(cnt, fullfilename, img, FTYPE_IMAGE_SNAPSHOT);
     }
