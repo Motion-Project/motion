6f76d55db227cc629e85f75cc175f1088d6699a6
Joseph Heenan <joseph@heenan.me.uk>
2016-10-06 09:48:57 +0100

    Tidy up netcam_rtsp.h
    - place lines not affected by HAVE_FFMPEG outside the ifdef
    - add include guards (for consistency with other .h files)

--- netcam_rtsp.h
+++ netcam_rtsp.h
@@ -1,3 +1,6 @@
+#ifndef _INCLUDE_NETCAM_RTSP_H
+#define _INCLUDE_NETCAM_RTSP_H
+
 #include "netcam.h"
 
 #ifdef HAVE_FFMPEG
@@ -25,13 +28,6 @@ struct rtsp_context {
     struct SwsContext*   swsctx;
 };
 
-struct rtsp_context *rtsp_new_context(void);
-void netcam_shutdown_rtsp(netcam_context_ptr netcam);
-int netcam_connect_rtsp(netcam_context_ptr netcam);
-int netcam_read_rtsp_image(netcam_context_ptr netcam);
-int netcam_setup_rtsp(netcam_context_ptr netcam, struct url_t *url);
-int netcam_next_rtsp(unsigned char *image , netcam_context_ptr netcam);
-
 #else /* Do not have FFmpeg */
 
 struct rtsp_context {
@@ -39,6 +35,8 @@ struct rtsp_context {
     enum RTSP_STATUS      status;
 };
 
+#endif /* end HAVE_FFMPEG  */
+
 struct rtsp_context *rtsp_new_context(void);
 void netcam_shutdown_rtsp(netcam_context_ptr netcam);
 int netcam_connect_rtsp(netcam_context_ptr netcam);
@@ -46,6 +44,4 @@ int netcam_read_rtsp_image(netcam_context_ptr netcam);
 int netcam_setup_rtsp(netcam_context_ptr netcam, struct url_t *url);
 int netcam_next_rtsp(unsigned char *image , netcam_context_ptr netcam);
 
-#endif /* end HAVE_FFMPEG  */
-
-
+#endif /* _INCLUDE_NETCAM_RTSP_H */
