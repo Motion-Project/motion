a8709463e700a8d96bbc79219e5b10f315d6d3a6
Joseph Heenan <joseph@emobix.co.uk>
2016-10-23 10:45:33 +0100

    Move from old GNU field designator to the standard C99 way
    The GNU style syntax has been marked as obsolete since gcc 2.5 I believe,
    which was released in 1993.
    
    Fixes some warnings in the macOS builds.

--- track.c
+++ track.c
@@ -17,25 +17,25 @@
 
 
 struct trackoptions track_template = {
-    dev:            -1,             /* dev open */
-    port:           NULL,           /* char *port */
-    motorx:         0,              /* int motorx */
-    motory:         0,              /* int motory */
-    maxx:           0,              /* int maxx; */
-    maxy:           0,              /* int maxy; */
-    minx:           0,              /* int minx; */
-    miny:           0,              /* int miny; */
-    homex:          128,            /* int homex; */
-    homey:          128,            /* int homey; */
-    motorx_reverse: 0,              /* int reversed x servo; */
-    motory_reverse: 0,              /* int reversed y servo; */
-    speed:          TRACK_SPEED,    /* speed */
-    stepsize:       TRACK_STEPSIZE, /* stepsize */
-    active:         0,              /* auto tracking active */
-    minmaxfound:    0,              /* flag for minmax values stored for pwc based camera */
-    step_angle_x:   10,             /* UVC step angle in degrees X-axis that camera moves during auto tracking */
-    step_angle_y:   10,             /* UVC step angle in degrees Y-axis that camera moves during auto tracking */
-    move_wait:      10              /* number of frames to disable motion detection after camera moving */
+    .dev =             -1,             /* dev open */
+    .port =            NULL,           /* char *port */
+    .motorx =          0,              /* int motorx */
+    .motory =          0,              /* int motory */
+    .maxx =            0,              /* int maxx; */
+    .maxy =            0,              /* int maxy; */
+    .minx =            0,              /* int minx; */
+    .miny =            0,              /* int miny; */
+    .homex =           128,            /* int homex; */
+    .homey =           128,            /* int homey; */
+    .motorx_reverse =  0,              /* int reversed x servo; */
+    .motory_reverse =  0,              /* int reversed y servo; */
+    .speed =           TRACK_SPEED,    /* speed */
+    .stepsize =        TRACK_STEPSIZE, /* stepsize */
+    .active =          0,              /* auto tracking active */
+    .minmaxfound =     0,              /* flag for minmax values stored for pwc based camera */
+    .step_angle_x =    10,             /* UVC step angle in degrees X-axis that camera moves during auto tracking */
+    .step_angle_y =    10,             /* UVC step angle in degrees Y-axis that camera moves during auto tracking */
+    .move_wait =       10              /* number of frames to disable motion detection after camera moving */
 };
 
 
