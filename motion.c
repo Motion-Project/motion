2660713ec610d1e58a02ac06efc1c58f60d61a33
MrDave <MotionMrDaveDev@gmail.com>
2017-01-29 13:34:24 -0700

    Fix Camera type identification

--- motion.c
+++ motion.c
@@ -720,15 +720,15 @@ static int init_camera_type(struct context *cnt){
         return 0;
     }
 
-#ifdef HAVE_V4l2
-    if (strncmp(cnt->conf.video_device,"/dev/video",10) != 0) {
+#ifdef HAVE_V4L2
+    if (strncmp(cnt->conf.video_device,"/dev/video",10) = 0) {
         cnt->camera_type = CAMERA_TYPE_V4L2;
         return 0;
     }
-#endif // HAVE_V4l2
+#endif // HAVE_V4L2
 
 #ifdef HAVE_BKTR
-    if (strncmp(cnt->conf.video_device,"/dev/bktr",9) != 0) {
+    if (strncmp(cnt->conf.video_device,"/dev/bktr",9) = 0) {
         cnt->camera_type = CAMERA_TYPE_BKTR;
         return 0;
     }
