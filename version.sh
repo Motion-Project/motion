79060d0590e0730ce22d1cc833772869473a3557
MrDave <MotionMrDaveDev@gmail.com>
2017-11-08 20:23:21 -0700

    Version Script

--- version.sh
+++ version.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-BASE_VERSION="4.0.1"
+BASE_VERSION="4.1"
 if [ -d .git ]; then
-	GIT_COMMIT=`git show -s --format=%h`
-	printf "$BASE_VERSION+git$GIT_COMMIT"
+    GIT_COMMIT=`git show -s --date=format:'%Y%m%d' --format=%cd-%h`
+    printf "$BASE_VERSION"
+    #printf "$BASE_VERSION+git$GIT_COMMIT"
 else
-	printf "$BASE_VERSION+gitUNKNOWN"
+    printf "$BASE_VERSION"
+    #printf "$BASE_VERSION+gitUNKNOWN"
 fi
