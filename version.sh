293be40df04443809f624fae772c52e90d2fb30c
MrDave <MotionMrDaveDev@gmail.com>
2017-12-05 19:18:27 -0700

    Update version script for old distros

--- version.sh
+++ version.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 BASE_VERSION="4.1"
 if [ -d .git ]; then
-    GIT_COMMIT=`git show -s --date=format:'%Y%m%d' --format=%cd-%h`
+    GIT_COMMIT=`git show -s --date=format:'%Y%m%d' --format=%cd-%h` 2>/dev/null
+    if [ $? -ne 0 ]; then
+        GIT_COMMIT=`git show -s --format=%h`
+    fi
     #printf "$BASE_VERSION"
     printf "$BASE_VERSION+git$GIT_COMMIT"
 else
