804c28cb04606494a680e4033276e53522542d68
Mr-DaveDev <MotionMrDaveDev@gmail.com>
2017-12-16 11:39:32 -0700

    Default Webp to be included in builds
    * Change default for Webp image types
    
    * Guide update for webp
    
    * Revise Webp defaults and add to testing

--- test_builds.sh
+++ test_builds.sh
@@ -107,6 +107,23 @@ fi
 SUMMARY=$SUMMARY"\n Option(s): "$OPTION" Config: "$RESULT_CONFIG" make: "$RESULT_MAKE
 
 ##################################################################################################
+OPTION=" --with-developer-flags --without-webp "
+RESULT_CONFIG="Failed"
+RESULT_MAKE="Failed"
+printf "\n\n$STARS Testing $OPTION $STARS\n\n"
+if ./configure $OPTION; then
+  RESULT_CONFIG="OK"
+  if make; then
+    RESULT_MAKE="OK"
+  else
+    RESULT="FAIL"
+  fi
+else
+  RESULT="FAIL"
+fi
+SUMMARY=$SUMMARY"\n Option(s): "$OPTION" Config: "$RESULT_CONFIG" make: "$RESULT_MAKE
+
+##################################################################################################
 OPTION=" --with-developer-flags --without-mysql --without-sqlite3 --without-pgsql "
 RESULT_CONFIG="Failed"
 RESULT_MAKE="Failed"
