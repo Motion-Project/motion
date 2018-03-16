d67327f11137709877e285f7c39b6c28441b35d6
MrDave <MotionMrDaveDev@gmail.com>
2018-03-15 22:03:20 -0600

    Add gettext

--- translate.c
+++ translate.c
@@ -2,7 +2,7 @@
 /*
  *    translate.c
  *
- *    Rudimentary translations for Web User control interface.
+ *    Translations for Web User control interface.
  *
  *    This software is distributed under the GNU Public License Version 2
  *    See also the file 'COPYING'.
@@ -13,187 +13,34 @@
 #include "motion.h"
 #include "translate.h"
 
-struct trns_ctx{
-  const char *lang;
-  const char *en_word;
-  const char *fx_word;
-};
+void translate_locale_chg(const char *langcd){
+#ifdef HAVE_INTL
 
-/** ABSOLUTELY NO QUOTES PERMITED IN THE TRANSLATED VALUES!!!*/
+    setenv ("LANGUAGE", langcd, 1);
+    /* Invoke external function to change locale*/
+    ++_nl_msg_cat_cntr;
 
-struct trns_ctx trns_phrases[] = {
-     {"en","english word","foreign word"}
-
-/* Spanish */
-    ,{"es","All Cameras","Todas Cámaras"}
-    ,{"es","All","Todas"}
-    ,{"es","Action","Acción"}
-    ,{"es","Make Movie","Hacer Pelicula"}
-    ,{"es","Snapshot","Instantánea"}
-    ,{"es","Cameras","Cámaras"}
-    ,{"es","Camera","Cámara"}
-    ,{"es","Change Configuration","Cambiar Configuración"}
-    ,{"es","Write Configuration","Configuración de Escritura"}
-    ,{"es","Tracking","Rastreo"}
-    ,{"es","Pause","Pausa"}
-    ,{"es","Start","Comienzo"}
-    ,{"es","Restart","Reiniciar"}
-    ,{"es","Quit","Dejar"}
-    ,{"es","Help","Ayuda"}
-    ,{"es","pan/tilt","Girar/Inclinación"}
-    ,{"es","pan","Girar"}
-    ,{"es","tilt","Inclinación"}
-    ,{"es","Absolute Change","Cambio Absoluto"}
-    ,{"es","Center","Centro"}
-    ,{"es","save","Salvar"}
-    ,{"es","select option","Seleccionar opción"}
-    ,{"es","No Configuration Options","Sin opciones de configuración"}
-    ,{"es","Limited Configuration Options","Opciones de configuración limitadas"}
-    ,{"es","Advanced Configuration Options","Opciones de configuración avanzada"}
-    ,{"es","Restricted Configuration Options","Opciones de configuración restringida"}
-    ,{"es","setup_mode","modo de configuración"}
-
-/* Lithuanian */
-    ,{"lt","All Cameras","Visos kameros"}
-    ,{"lt","All","Visi"}
-    ,{"lt","Action","Veiksmas"}
-    ,{"lt","Make Movie","Įrašyti video"}
-    ,{"lt","Snapshot","Nuotrauka"}
-    ,{"lt","Cameras","Kameros"}
-    ,{"lt","Camera","Kamera"}
-    ,{"lt","Change Configuration","Keisti nustatymus"}
-    ,{"lt","Write Configuration","Išsaugoti nustatymus"}
-    ,{"lt","Tracking","Sekimas"}
-    ,{"lt","Pause","Pauzė"}
-    ,{"lt","Start","Pradėti"}
-    ,{"lt","Restart","Perkrauti"}
-    ,{"lt","Quit","Išeiti"}
-    ,{"lt","Help","Pagalba"}
-    ,{"lt","pan/tilt","Pasukti/Palenkti"}
-    ,{"lt","pan","Pasukti"}
-    ,{"lt","tilt","Palenkti"}
-    ,{"lt","Absolute Change","Absoliutus skirtumas"}
-    ,{"lt","Center","Centras"}
-    ,{"lt","save","Išsaugoti"}
-    ,{"lt","select option","Pasirinkite parametrą"}
-    ,{"lt","No Configuration Options","Nėra konfigūracijos parametrų"}
-    ,{"lt","Limited Configuration Options","Riboti konfigūracijos parametrai"}
-    ,{"lt","Advanced Configuration Options","Išplėstiniai konfigūracijos parametrai"}
-    ,{"lt","Restricted Configuration Options","Apriboti konfigūracijos parametrai"}
-    ,{"lt","setup_mode","Konfigūravimo režimas"}
-
-/* Dutch */
-    ,{"nl","All Cameras","Alle Cameras"}
-    ,{"nl","All","Alle"}
-    ,{"nl","Action","Actie"}
-    ,{"nl","Make Movie","Film Maken"}
-    ,{"nl","Snapshot","Momentopname"}
-    ,{"nl","Cameras","Cameras"}
-    ,{"nl","Camera","Fotoaparatas"}
-    ,{"nl","Change Configuration","Configuratie wijzigen"}
-    ,{"nl","Write Configuration","Schrijf Configuratie"}
-    ,{"nl","Tracking","Bijhouden"}
-    ,{"nl","Pause","Pauze"}
-    ,{"nl","Start","Begin"}
-    ,{"nl","Restart","Herstarten"}
-    ,{"nl","Quit","Ophouden"}
-    ,{"nl","Help","Helpen"}
-    ,{"nl","pan/tilt","Draaien/Kantelen"}
-    ,{"nl","pan","Draaien"}
-    ,{"nl","tilt","Kantelen"}
-    ,{"nl","Absolute Change","Absolute Verandering"}
-    ,{"nl","Center","Middelpunt"}
-    ,{"nl","save","Opslaan"}
-    ,{"nl","select option","Selecteer optie"}
-    ,{"nl","No Configuration Options","Geen configuratie-opties"}
-    ,{"nl","Limited Configuration Options","Enigszins Beperkte configuratie-opties"}
-    ,{"nl","Advanced Configuration Options","Geavanceerde configuratie-opties"}
-    ,{"nl","Restricted Configuration Options","Zeer Beperkte configuratie-opties"}
-    ,{"nl","setup_mode","instelmodus"}
-
-/* French */
-    ,{"fr","All Cameras","Toutes les caméras"}
-    ,{"fr","All","Tout"}
-    ,{"fr","Action","Action"}
-    ,{"fr","Make Movie","Faire un film"}
-    ,{"fr","Snapshot","Instantané"}
-    ,{"fr","Cameras","Caméras"}
-    ,{"fr","Camera","Caméra"}
-    ,{"fr","Change Configuration","Modifier la configuration"}
-    ,{"fr","Write Configuration","Ecrire la configuration"}
-    ,{"fr","Tracking","Suivi"}
-    ,{"fr","Pause","Pause"}
-    ,{"fr","Start","Début"}
-    ,{"fr","Restart","Redémarrer"}
-    ,{"fr","Quit","Quitter"}
-    ,{"fr","Help","Aidez-moi"}
-    ,{"fr","pan/tilt","Pivot/Inclinaison"}
-    ,{"fr","pan","pivot"}
-    ,{"fr","tilt","Inclinaison"}
-    ,{"fr","Absolute Change","Changement Absolu"}
-    ,{"fr","Center","Centre"}
-    ,{"fr","save","Sauvegarder"}
-    ,{"fr","select option","sélectionnez option"}
-    ,{"fr","No Configuration Options","Aucune option de configuration"}
-    ,{"fr","Limited Configuration Options","Options de configuration limitées"}
-    ,{"fr","Advanced Configuration Options","Options de configuration avancées"}
-    ,{"fr","Restricted Configuration Options","Options de configuration restreintes"}
-    ,{"fr","setup_mode","Mode de configuration"}
-
-/* Swedish */
-    ,{"sv","All Cameras","Alla kameror"}
-    ,{"sv","All","Alla"}
-    ,{"sv","Action","Åtgärd"}
-    ,{"sv","Make Movie","Gör en film"}
-    ,{"sv","Snapshot","Ögonblicksbild"}
-    ,{"sv","Cameras","Kameror"}
-    ,{"sv","Camera","Kamera"}
-    ,{"sv","Change Configuration","Ändra konfiguration"}
-    ,{"sv","Write Configuration","Skriv konfiguration"}
-    ,{"sv","Tracking","Spårning"}
-    ,{"sv","Pause","Paus"}
-    ,{"sv","Start","Start"}
-    ,{"sv","Restart","Starta om"}
-    ,{"sv","Quit","Avsluta"}
-    ,{"sv","Help","Hjälp"}
-    ,{"sv","pan/tilt","panorera/luta"}
-    ,{"sv","pan","panorera"}
-    ,{"sv","tilt","luta"}
-    ,{"sv","Absolute Change","Absolut ändring"}
-    ,{"sv","Center","Centrera"}
-    ,{"sv","save","spara"}
-    ,{"sv","select option","välj alternativ"}
-    ,{"sv","No Configuration Options","Inga konfigurationsalternativ"}
-    ,{"sv","Limited Configuration Options","Reducerade konfigurationsalternativ"}
-    ,{"sv","Advanced Configuration Options","Avancerade konfigurationsalternativ"}
-    ,{"sv","Restricted Configuration Options","Begränsade konfigurationsalternativ"}
-    ,{"sv","setup_mode","inställningsläge"}
-
-/* Next Language */
-
-/* Termination Entry */
-    ,{NULL,NULL,NULL}
-
-};
-
-
-void translate_word(const char *en_word, char *fx_word, int maxsize, char *lang) {
-
-
-    int indx;
-
-    /* Set default to english word */
-    snprintf(fx_word, maxsize, "%s", en_word);
-
-    indx = 0;
-    while (trns_phrases[indx].lang != NULL){
-        if ((!strcasecmp(lang, trns_phrases[indx].lang)) &&
-            (!strcasecmp(en_word, trns_phrases[indx].en_word))){
-            snprintf(fx_word, maxsize,"%s",trns_phrases[indx].fx_word);
-        }
-        indx++;
-    }
-
-    return;
-
+#else
+    if (langcd != NULL) MOTION_LOG(NTC, TYPE_ALL, NO_ERRNO,"No native language support");
+#endif
 }
+
+void translate_init(void){
+#ifdef HAVE_INTL
+
+    setlocale (LC_ALL, "");
+
+    //translate_locale_chg("li");
+
+    bindtextdomain ("motion", LOCALEDIR);
+    bind_textdomain_codeset ("motion", "UTF-8");
+    textdomain ("motion");
+
+    MOTION_LOG(NTC, TYPE_ALL, NO_ERRNO,"%s",_("Language: English"));
+
+#else
+    /* This avoids a unused function warning */
+    translate_locale_chg("en");
+#endif
+}
+
--- translate.h
+++ translate.h
@@ -10,7 +10,20 @@
 #ifndef _INCLUDE_TRANSLATE_H_
 #define _INCLUDE_TRANSLATE_H_
 
-    void translate_word(const char *en_word, char *fx_word, int maxsize, char *lang);
+
+#ifdef HAVE_INTL
+
+#   include <libintl.h>
+#   define _(STRING) gettext(STRING)
+
+    /* Required for changing the locale dynamically */
+    extern int  _nl_msg_cat_cntr;
+#else
+#   define _(STRING) STRING
+#endif
+
+    void translate_init(void);
+    void translate_locale_chg(const char *langcd);
 
 #endif // _INCLUDE_TRANSLATE_H_
 
