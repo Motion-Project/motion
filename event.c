a0816e72f02588402fd8c0e0a4227d63968bca3f
sander1234567890 <11135993+sander1234567890@users.noreply.github.com>
2018-03-25 23:01:57 +0200

    Event timestamp and SQL improvements (#668)
    Use the same timestamp for FILECREATE and FILECLOSE events
    
    Propagate timestamp to the FILECREATE and FILECLOSE events, so the
    timestamp available for the "Conversion Specifiers" for SQL queries
    matches the timestamp used in filenames.
    
    Move FILE_CREATE event call from picture.c:put_picture() to its calling functions in event.c.
    
    Add new option to execute a SQL query on the FILECLOSE event
    
    Don't try to execute empty sqlquery strings
    
    Always clear result with PQclear
    
    Improve Postgresql error handling
    
    Don't regard PGRES_TUPLES_OK status as error.
    
    Print Postgresql error status and error message on error.

--- event.c
+++ event.c
@@ -153,6 +153,12 @@ static void on_motion_detected_command(struct context *cnt,
 
 static void do_sql_query(char *sqlquery, struct context *cnt, int save_id)
 {
+
+    if (strlen(sqlquery) <= 0) {
+        /* don't try to execute empty queries */
+        return;
+    }
+
 #ifdef HAVE_MYSQL
     if (!strcmp(cnt->conf.database_type, "mysql")) {
         if (mysql_query(cnt->database, sqlquery) != 0) {
@@ -217,14 +223,16 @@ static void do_sql_query(char *sqlquery, struct context *cnt, int save_id)
                            cnt->conf.database_dbname);
             }
 
-        } else if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-            MOTION_LOG(ERR, TYPE_DB, SHOW_ERRNO, "PGSQL query [%s] failed", sqlquery);
-            PQclear(res);
+        } else if (!(PQresultStatus(res) == PGRES_COMMAND_OK || PQresultStatus(res) == PGRES_TUPLES_OK)) {
+            MOTION_LOG(ERR, TYPE_DB, SHOW_ERRNO, "PGSQL query failed: [%s]  %s %s",
+                       sqlquery, PQresStatus(PQresultStatus(res)), PQresultErrorMessage(res));
         }
         if (save_id) {
             //ToDO:  Find the equivalent option for pgsql
             cnt->database_event_id = 0;
         }
+
+        PQclear(res);
     }
 #endif /* HAVE_PGSQL */
 
@@ -273,7 +281,7 @@ static void event_sqlfirstmotion(struct context *cnt, motion_event type  ATTRIBU
 
 static void event_sqlnewfile(struct context *cnt, motion_event type  ATTRIBUTE_UNUSED,
             struct image_data *dummy ATTRIBUTE_UNUSED,
-            char *filename, void *arg, struct timeval *tv1 ATTRIBUTE_UNUSED)
+            char *filename, void *arg, struct timeval *currenttime_tv)
 {
     int sqltype = (unsigned long)arg;
 
@@ -289,7 +297,31 @@ static void event_sqlnewfile(struct context *cnt, motion_event type  ATTRIBUTE_U
         char sqlquery[PATH_MAX];
 
         mystrftime(cnt, sqlquery, sizeof(sqlquery), cnt->conf.sql_query,
-                   &cnt->current_image->timestamp_tv, filename, sqltype);
+                   currenttime_tv, filename, sqltype);
+
+        do_sql_query(sqlquery, cnt, 0);
+    }
+}
+
+static void event_sqlfileclose(struct context *cnt, motion_event type  ATTRIBUTE_UNUSED,
+            struct image_data *dummy ATTRIBUTE_UNUSED,
+            char *filename, void *arg, struct timeval *currenttime_tv)
+{
+    int sqltype = (unsigned long)arg;
+
+    /* Only log the file types we want */
+    if (!(cnt->conf.database_type) || (sqltype & cnt->sql_mask) == 0)
+        return;
+
+    /*
+     * We place the code in a block so we only spend time making space in memory
+     * for the sqlquery and timestr when we actually need it.
+     */
+    {
+        char sqlquery[PATH_MAX];
+
+        mystrftime(cnt, sqlquery, sizeof(sqlquery), cnt->conf.sql_query_stop,
+                   currenttime_tv, filename, sqltype);
 
         do_sql_query(sqlquery, cnt, 0);
     }
@@ -408,6 +440,7 @@ static void event_image_detect(struct context *cnt,
             put_picture(cnt, fullfilename,img_data->image_norm, FTYPE_IMAGE);
         }
 
+        event(cnt, EVENT_FILECREATE, NULL, fullfilename, (void *)FTYPE_IMAGE, currenttime_tv);
     }
 }
 
@@ -440,6 +473,7 @@ static void event_imagem_detect(struct context *cnt,
         snprintf(fullfilenamem, PATH_MAX, "%s/%s.%s", cnt->conf.filepath, filenamem, imageext(cnt));
 
         put_picture(cnt, fullfilenamem, cnt->imgs.img_motion.image_norm, FTYPE_IMAGE_MOTION);
+        event(cnt, EVENT_FILECREATE, NULL, fullfilenamem, (void *)FTYPE_IMAGE, currenttime_tv);
     }
 }
 
@@ -473,6 +507,7 @@ static void event_image_snapshot(struct context *cnt,
         snprintf(filename, PATH_MAX, "%s.%s", filepath, imageext(cnt));
         snprintf(fullfilename, PATH_MAX, "%s/%s", cnt->conf.filepath, filename);
         put_picture(cnt, fullfilename, img_data->image_norm, FTYPE_IMAGE_SNAPSHOT);
+        event(cnt, EVENT_FILECREATE, NULL, fullfilename, (void *)FTYPE_IMAGE, currenttime_tv);
 
         /*
          *  Update symbolic link *after* image has been written so that
@@ -492,6 +527,7 @@ static void event_image_snapshot(struct context *cnt,
         snprintf(fullfilename, PATH_MAX, "%s/%s", cnt->conf.filepath, filename);
         remove(fullfilename);
         put_picture(cnt, fullfilename, img_data->image_norm, FTYPE_IMAGE_SNAPSHOT);
+        event(cnt, EVENT_FILECREATE, NULL, fullfilename, (void *)FTYPE_IMAGE, currenttime_tv);
     }
 
     cnt->snapshot = 0;
@@ -529,7 +565,7 @@ static void on_movie_end_command(struct context *cnt,
 static void event_extpipe_end(struct context *cnt,
             motion_event type ATTRIBUTE_UNUSED,
             struct image_data *dummy ATTRIBUTE_UNUSED, char *dummy1 ATTRIBUTE_UNUSED,
-            void *dummy2 ATTRIBUTE_UNUSED, struct timeval *tv1 ATTRIBUTE_UNUSED)
+            void *dummy2 ATTRIBUTE_UNUSED, struct timeval *currenttime_tv)
 {
     if (cnt->extpipe_open) {
         cnt->extpipe_open = 0;
@@ -538,7 +574,7 @@ static void event_extpipe_end(struct context *cnt,
                    fileno(cnt->extpipe), ferror(cnt->extpipe));
         MOTION_LOG(NTC, TYPE_EVENTS, NO_ERRNO, "pclose return: %d",
                    pclose(cnt->extpipe));
-        event(cnt, EVENT_FILECLOSE, NULL, cnt->extpipefilename, (void *)FTYPE_MPEG, NULL);
+        event(cnt, EVENT_FILECLOSE, NULL, cnt->extpipefilename, (void *)FTYPE_MPEG, currenttime_tv);
     }
 }
 
@@ -595,7 +631,7 @@ static void event_create_extpipe(struct context *cnt,
 
         MOTION_LOG(NTC, TYPE_EVENTS, NO_ERRNO, "cnt->moviefps: %d", cnt->movie_fps);
 
-        event(cnt, EVENT_FILECREATE, NULL, cnt->extpipefilename, (void *)FTYPE_MPEG, NULL);
+        event(cnt, EVENT_FILECREATE, NULL, cnt->extpipefilename, (void *)FTYPE_MPEG, currenttime_tv);
         cnt->extpipe = popen(stamp, "w");
 
         if (cnt->extpipe == NULL) {
@@ -781,7 +817,7 @@ static void event_ffmpeg_newfile(struct context *cnt,
             cnt->ffmpeg_output=NULL;
             return;
         }
-        event(cnt, EVENT_FILECREATE, NULL, cnt->newfilename, (void *)FTYPE_MPEG, NULL);
+        event(cnt, EVENT_FILECREATE, NULL, cnt->newfilename, (void *)FTYPE_MPEG, currenttime_tv);
     }
 
     if (cnt->conf.ffmpeg_output_debug) {
@@ -901,7 +937,7 @@ static void event_ffmpeg_timelapse(struct context *cnt,
             cnt->ffmpeg_timelapse = NULL;
             return;
         }
-        event(cnt, EVENT_FILECREATE, NULL, cnt->timelapsefilename, (void *)FTYPE_MPEG_TIMELAPSE, NULL);
+        event(cnt, EVENT_FILECREATE, NULL, cnt->timelapsefilename, (void *)FTYPE_MPEG_TIMELAPSE, currenttime_tv);
     }
 
     if (ffmpeg_put_image(cnt->ffmpeg_timelapse, img_data, currenttime_tv) == -1) {
@@ -931,21 +967,21 @@ static void event_ffmpeg_closefile(struct context *cnt,
             motion_event type ATTRIBUTE_UNUSED,
             struct image_data *dummy1 ATTRIBUTE_UNUSED,
             char *dummy2 ATTRIBUTE_UNUSED, void *dummy3 ATTRIBUTE_UNUSED,
-            struct timeval *tv1 ATTRIBUTE_UNUSED)
+            struct timeval *currenttime_tv)
 {
 
     if (cnt->ffmpeg_output) {
         ffmpeg_close(cnt->ffmpeg_output);
         free(cnt->ffmpeg_output);
         cnt->ffmpeg_output = NULL;
-        event(cnt, EVENT_FILECLOSE, NULL, cnt->newfilename, (void *)FTYPE_MPEG, NULL);
+        event(cnt, EVENT_FILECLOSE, NULL, cnt->newfilename, (void *)FTYPE_MPEG, currenttime_tv);
     }
 
     if (cnt->ffmpeg_output_debug) {
         ffmpeg_close(cnt->ffmpeg_output_debug);
         free(cnt->ffmpeg_output_debug);
         cnt->ffmpeg_output_debug = NULL;
-        event(cnt, EVENT_FILECLOSE, NULL, cnt->motionfilename, (void *)FTYPE_MPEG_MOTION, NULL);
+        event(cnt, EVENT_FILECLOSE, NULL, cnt->motionfilename, (void *)FTYPE_MPEG_MOTION, currenttime_tv);
     }
 
 }
@@ -954,13 +990,13 @@ static void event_ffmpeg_timelapseend(struct context *cnt,
             motion_event type ATTRIBUTE_UNUSED,
             struct image_data *dummy1 ATTRIBUTE_UNUSED,
             char *dummy2 ATTRIBUTE_UNUSED, void *dummy3 ATTRIBUTE_UNUSED,
-            struct timeval *tv1 ATTRIBUTE_UNUSED)
+            struct timeval *currenttime_tv)
 {
     if (cnt->ffmpeg_timelapse) {
         ffmpeg_close(cnt->ffmpeg_timelapse);
         free(cnt->ffmpeg_timelapse);
         cnt->ffmpeg_timelapse = NULL;
-        event(cnt, EVENT_FILECLOSE, NULL, cnt->timelapsefilename, (void *)FTYPE_MPEG_TIMELAPSE, NULL);
+        event(cnt, EVENT_FILECLOSE, NULL, cnt->timelapsefilename, (void *)FTYPE_MPEG_TIMELAPSE, currenttime_tv);
     }
 }
 
@@ -1070,6 +1106,12 @@ struct event_handlers event_handlers[] = {
     EVENT_TIMELAPSEEND,
     event_ffmpeg_timelapseend
     },
+#if defined(HAVE_MYSQL) || defined(HAVE_PGSQL) || defined(HAVE_SQLITE3)
+    {
+    EVENT_FILECLOSE,
+    event_sqlfileclose
+    },
+#endif
     {
     EVENT_FILECLOSE,
     on_movie_end_command
