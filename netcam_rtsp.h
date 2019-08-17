8f556e9e27e85c2971faae049a3289db1950be20
Mr-DaveDev <MotionMrDaveDev@gmail.com>
2019-08-17 16:34:27 -0600

    Revise Netcam handler (#984)
    * Eliminate reconnect delay

--- netcam_rtsp.h
+++ netcam_rtsp.h
@@ -73,7 +73,6 @@ struct rtsp_context {
     int                       rtsp_uses_tcp;    /* Flag from config for whether to use tcp transport */
     int                       v4l2_palette;     /* Palette from config for v4l2 devices */
     int                       framerate;        /* Frames per second from configuration file */
-    long                      cnct_delay;       /* Delay offset to prevent handler being too slow*/
     int                       reconnect_count;  /* Count of the times reconnection is tried*/
     int                       src_fps;          /* The fps provided from source*/
 
