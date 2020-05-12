10a98441040c5d1ef745bdca18e80257f35b2d48
tosiara <tosiara@users.noreply.github.com>
2020-05-12 15:49:25 +0300

    Updated copyright to 2020

--- src/conf.c
+++ src/conf.c
@@ -3139,7 +3139,7 @@ static struct context **config_camera(struct context **cnt, const char *str,
  */
 static void usage()
 {
-    printf("motion Version "VERSION", Copyright 2000-2019 Jeroen Vreeken/Folkert van Heusden/Kenneth Lavrsen/Motion-Project maintainers\n");
+    printf("motion Version "VERSION", Copyright 2000-2020 Jeroen Vreeken/Folkert van Heusden/Kenneth Lavrsen/Motion-Project maintainers\n");
     printf("\nHome page :\t https://motion-project.github.io/ \n");
     printf("\nusage:\tmotion [options]\n");
     printf("\n\n");
