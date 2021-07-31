219cc9eb01e063b2dc105ed33ef0a5b09cb8d3c8
PeterHolik <>
2021-07-30 22:50:56 -0600

    Remove work-around for exif format

--- src/picture.c
+++ src/picture.c
@@ -185,7 +185,6 @@ unsigned prepare_exif(unsigned char **exif, const struct context *cnt
     */
     char *description, *datetime, *subtime;
     char datetime_buf[22];
-    char tmpbuf[45];
     struct tm timestamp_tm;
     struct timeval tv1;
 
@@ -197,19 +196,13 @@ unsigned prepare_exif(unsigned char **exif, const struct context *cnt
 
     localtime_r(&tv1.tv_sec, &timestamp_tm);
     /* Exif requires this exact format */
-    /* The compiler is twitchy on truncating formats and the exif is twitchy
-     * on the length of the whole string.  So we do it in two steps of printing
-     * into a large buffer which compiler wants, then print that into the smaller
-     * buffer that exif wants..TODO  Find better method
-     */
-    snprintf(tmpbuf, 45, "%04d:%02d:%02d %02d:%02d:%02d",
-            timestamp_tm.tm_year + 1900,
-            timestamp_tm.tm_mon + 1,
-            timestamp_tm.tm_mday,
-            timestamp_tm.tm_hour,
-            timestamp_tm.tm_min,
-            timestamp_tm.tm_sec);
-    snprintf(datetime_buf, 22,"%.21s",tmpbuf);
+    snprintf(datetime_buf, 21, "%04d:%02d:%02d %02d:%02d:%02d",
+        (timestamp_tm.tm_year + 1900) & 0xffff,
+        (timestamp_tm.tm_mon + 1) & 0x0f,
+        timestamp_tm.tm_mday,
+        timestamp_tm.tm_hour,
+        timestamp_tm.tm_min,
+        timestamp_tm.tm_sec);
     datetime = datetime_buf;
 
     // TODO: Extract subsecond timestamp from somewhere, but only
