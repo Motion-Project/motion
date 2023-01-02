5568459ffa0e7af32f0ea8c5ca5320c9d73105dc
Mr-Dave <MotionMrDave@gmail.com>
2023-01-02 11:32:46 -0700

    Update year

--- src/event.cpp
+++ src/event.cpp
@@ -14,7 +14,7 @@
  *    You should have received a copy of the GNU General Public License
  *    along with MotionPlus.  If not, see <https://www.gnu.org/licenses/>.
  *
- *    Copyright 2020-2022 MotionMrDave@gmail.com
+ *    Copyright 2020-2023 MotionMrDave@gmail.com
 */
 
 #include "motionplus.hpp"
