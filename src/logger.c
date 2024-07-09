4c32543a2694f5574fa6099a9b32445d02e91b19
Mr-Dave <MotionMrDave@gmail.com>
2024-07-09 17:48:43 -0600

    Revise maximum length for date time in log.  Closes #1777

--- src/logger.c
+++ src/logger.c
@@ -154,13 +154,11 @@ FILE * set_logfile(const char *logfile_name)
  */
 static char *str_time(void)
 {
-    //static char buffer[16]; // the month differs depending on the language. ex)LANG=ja_JP.UTF-8
-    static char buffer[18];
+    static char buffer[30]; /* Arbitrary length*/
     time_t now = 0;
 
     now = time(0);
-    //strftime(buffer, 16, "%b %d %H:%M:%S", localtime(&now));
-    strftime(buffer, 18, "%b %d %H:%M:%S", localtime(&now));
+    strftime(buffer, sizeof(buffer), "%b %d %H:%M:%S", localtime(&now));
     return buffer;
 }
 
